0f5a074f6b4199469284b492da4371eb8a9eb5eb
Pablo <pablma11@ucm.es>
2019-10-14 19:13:31 +0200

    cost optimized

--- Automatic Learning/Mandatory 2/Practica2.py
+++ Automatic Learning/Mandatory 2/Practica2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pandas.io.parsers import read_csv
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
 
 
 def main():
@@ -9,12 +10,19 @@ def main():
     grades = get_grades(data1)
     admissions = get_admission(data1)
 
-    theta = np.zeros(grades.shape[1] + 1)
+    theta = np.zeros(grades.shape[1] + 1) 
 
     grades = np.hstack([np.ones([grades.shape[0], 1]), grades])
 
-    print(gradient(theta, grades, admissions))
-    print(cost(theta, grades, admissions))
+    #_gradient = gradient(theta, grades, admissions)
+    #_cost = cost(theta, grades, admissions)
+
+    opt_theta = optimize_params(theta, grades, admissions)     
+    
+    _cost = cost(opt_theta, grades, admissions)
+    print(_cost)
+    
+    #get_graph(grades, admissions, opt_theta)  
 
 
 # Function to load a csv file
@@ -37,7 +45,9 @@ def get_admission(data):
     return data[:, -1]
      
 
-def get_graph(data, grades, admissions):
+def get_graph(grades, admissions, theta):
+
+    gr = grades[:, 1:grades.shape[1]]
 
     # gets an array with the index of the positive examples
     pos = np.where(admissions == 1)
@@ -46,8 +56,9 @@ def get_graph(data, grades, admissions):
 
     plt.figure()
     # Draws the positive and negative examples
-    plt.scatter(grades[pos, 0], grades[pos, 1], marker='+', c='red')
-    plt.scatter(grades[neg, 0], grades[neg, 1], c='blue')
+    plt.scatter(gr[pos, 0], gr[pos, 1], marker='+', c='red')
+    plt.scatter(gr[neg, 0], gr[neg, 1], c='blue')
+    plt.plot([30, 90], [theta[0] + theta[1] * 30, theta[0] + theta[1] * 90])
     plt.show()
 
 
@@ -65,6 +76,13 @@ def gradient(theta, x_samples, y_samples):
     m = x_samples.shape[0]
 
     return (x_samples.T @ (sigmoid_func(x_samples @ theta) - y_samples)) / m
+
+
+def optimize_params(theta, x_samples, y_samples):
+    result = opt.fmin_tnc(func=cost, x0=theta, fprime=gradient, args=(x_samples, y_samples))
+    theta_opt = result[0]
+
+    return theta_opt
 
 
 main()
