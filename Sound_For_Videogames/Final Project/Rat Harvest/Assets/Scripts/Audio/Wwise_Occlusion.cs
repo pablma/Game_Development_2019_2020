60dce74b3303cde575aa764b2fd36cfbaae6383e
Pablo <pablma11@ucm.es>
2019-12-16 10:10:59 +0100

    Occlusion effect in the FlowerPot implemented

--- Sound_For_Videogames/Final Project/Rat Harvest/Assets/Scripts/Audio/Wwise_Occlusion.cs
+++ Sound_For_Videogames/Final Project/Rat Harvest/Assets/Scripts/Audio/Wwise_Occlusion.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Wwise_Occlusion : MonoBehaviour
 {
     [SerializeField] private GameObject audioListener;
-    [SerializeField] private GameObject sound_emitter;
     private string RTPC_LoPass = "RTPC_Occlusion_LoPass";
     private string RTPC_Volume = "RTPC_Occlusion_Volume";
     private string nameOfListener;
@@ -14,25 +13,25 @@ public class Wwise_Occlusion : MonoBehaviour
     void Start()
     {
         nameOfListener = audioListener.name;
-        AkSoundEngine.RegisterGameObj(sound_emitter);
+        AkSoundEngine.RegisterGameObj(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = audioListener.transform.position - sound_emitter.transform.position;
+        Vector3 dir = audioListener.transform.position - gameObject.transform.position;
         RaycastHit hitInfo;
-        bool hit = Physics.Raycast(sound_emitter.transform.position, dir, out hitInfo);
+        bool hit = Physics.Raycast(gameObject.transform.position, dir, out hitInfo);
 
         if (hit) {
             if (hitInfo.collider.gameObject.name != nameOfListener)
             {
                 Debug.Log("Doing occlusion");
-                AkSoundEngine.SetRTPCValue(RTPC_LoPass, 50, sound_emitter);
+                AkSoundEngine.SetRTPCValue(RTPC_LoPass, 50, gameObject);
                 //AkSoundEngine.SetRTPCValue(RTPC_Volume, 1, sound_emitter);
             }
             else {
-                AkSoundEngine.SetRTPCValue(RTPC_LoPass, 0, sound_emitter);
+                AkSoundEngine.SetRTPCValue(RTPC_LoPass, 0, gameObject);
                 //AkSoundEngine.SetRTPCValue(RTPC_Volume, 0, sound_emitter);
             }
         }
